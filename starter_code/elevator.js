ead7b106b15700e093712cc9b1103ac587500970
Anthony Bruscantini <ajbruscantini@gmail.com>
2017-02-06 19:01:45 +0100

    finito

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,93 @@
+/* jshint esversion: 6 */
+var _ = require('lodash');
+
 class Elevator {
   constructor(){
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.waitingList = [];
+    this.passengers = [];
+    this.direction = 'up';
+    this.destination = undefined;
+    this.updateTimer = null;
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    this.updateTimer = setInterval(()=>{this.update();}, 1000);
+  }
+  stop() {
+    clearInterval(this.updateTimer);
+  }
+  update() {
+    if (this.destination !== undefined){
+      if (this.direction === 'up'){
+        this.floorUp();
+      }
+      else {
+        this.floorDown();
+      }
+
+      this.log();
+      _.remove(this.requests, (val) => {
+        return val === this.floor;
+      });
+      this._passengersLeave();
+      this._passengersEnter();
+
+      if (this.floor === this.destination){
+        this.destination = undefined;
+      }
+    }
+
+    else {
+      this.destination = this.requests.pop();
+      if (this.destination !== undefined){
+        if (this.destination > this.floor) this.direction = 'up';
+        else this.direction = 'down';
+      }
+    }
+
+  }
+  _passengersEnter() {
+    let waitingHere = _.remove(this.waitingList, (val) => {
+      return this.floor === val.originFloor;
+    });
+
+    let newFloors = _.map(waitingHere, (val) => {
+      return val.destinationFloor;
+    });
+    this.passengers = _.concat(this.passengers, waitingHere);
+    this.requests = _.union(this.requests, newFloors);
+    _.forEach(waitingHere, (val) => {
+      console.log(val.name + " has entered the elevator.");
+    });
+  }
+  _passengersLeave() {
+    let personsLeaving = _.remove(this.passengers, (val) => {
+      return this.floor === val.destinationFloor;
+    });
+    _.forEach(personsLeaving, (val) => {
+      console.log(val.name + " has left the elevator.");
+    });
+  }
+  floorUp() {
+    this.direction = 'up';
+    this.floor = this.floor + 1 > this.MAXFLOOR ? this.MAXFLOOR : this.floor + 1;
+  }
+  floorDown() {
+    this.direction = 'down';
+    this.floor = (this.floor - 1) < 0 ? 0 : this.floor - 1;
+  }
+  call(person) {
+    this.waitingList.push(person);
+    if (!_.includes(this.requests, person.originFloor)){
+      this.requests.push(person.originFloor);
+    }
+  }
+  log() {
+    console.log("Direction: " + this.direction + " | Floor: " + this.floor);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,18 @@
+/* jshint esversion: 6 */
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+var _ = require('lodash');
+
+var elevator = new Elevator();
+var persons = [
+  new Person('bob', 1, 4),
+  new Person('bill', 2, 3),
+  new Person('lucas', 10, 9),
+  new Person('nancy', 2, 1),
+  new Person('alice', 8, 2)
+];
+
+_.forEach(persons, (val) => {
+  elevator.call(val);
+});
+elevator.start();
